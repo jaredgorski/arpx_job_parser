e21cf318b164507b92b30d9a538e2c9e53aaeb27
jaredgorski <jared.gorski@liferay.cloud>
2022-02-14 15:00:19 -0800

    Update API, add test for parse_job

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod arpx_job;
+pub mod arpx_job;
 mod generic;
 
 mod prelude {
@@ -14,9 +14,78 @@ mod prelude {
     pub use crate::generic::combinators::right::right;
 }
 
-pub use arpx_job::{job, task, Job, Process, Task};
+pub use arpx_job::{Job, Process, Task};
 pub use generic::combinators::parser::{ParseResult, Parser};
 
-pub fn parse_job<'a>(job: &str) -> ParseResult<Job> {
+pub fn parse_job(job: &str) -> ParseResult<arpx_job::Job> {
     arpx_job::job().parse(job)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{parse_job, Job, Process, Task};
+
+    #[test]
+    fn test_parse_job() -> Result<(), String> {
+        let example = r#"
+            [
+                (loop1 ? loop2 : loop3;)
+                loop2 ? loop3 : loop4;
+            ]
+            loop3 ? loop4 : loop5;
+            loop6;
+            (loop7 ? loop8;)
+        "#;
+
+        let expected = (
+            "",
+            Job {
+                tasks: vec![
+                    Task {
+                        processes: vec![
+                            Process {
+                                name: "loop1".to_string(),
+                                onsucceed: "loop2".to_string(),
+                                onfail: "loop3".to_string(),
+                                silent: true,
+                            },
+                            Process {
+                                name: "loop2".to_string(),
+                                onsucceed: "loop3".to_string(),
+                                onfail: "loop4".to_string(),
+                                silent: false,
+                            },
+                        ],
+                    },
+                    Task {
+                        processes: vec![Process {
+                            name: "loop3".to_string(),
+                            onsucceed: "loop4".to_string(),
+                            onfail: "loop5".to_string(),
+                            silent: false,
+                        }],
+                    },
+                    Task {
+                        processes: vec![Process {
+                            name: "loop6".to_string(),
+                            onsucceed: String::new(),
+                            onfail: String::new(),
+                            silent: false,
+                        }],
+                    },
+                    Task {
+                        processes: vec![Process {
+                            name: "loop7".to_string(),
+                            onsucceed: "loop8".to_string(),
+                            onfail: String::new(),
+                            silent: true,
+                        }],
+                    },
+                ],
+            },
+        );
+
+        assert_eq!(parse_job(example)?, expected);
+        Ok(())
+    }
 }
